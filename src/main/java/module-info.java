e11544245e9957e2a963688fcf7da7b5ecb6e288
SamuelUmana <su4872165@gmail.com>
2026-06-09 21:04:32 -0600

    Interfaz de Datos Usuario Completo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ues.ues.salud {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ues.ues.salud to javafx.fxml;
     exports ues.ues.salud;
--- src/main/java/ues/ues/salud/PrimaryController.java
+++ src/main/java/ues/ues/salud/PrimaryController.java
@@ -1,12 +1,44 @@
 package ues.ues.salud;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.*;
 
-public class PrimaryController {
+
+public class PrimaryController implements Initializable{
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Button btnRegistro;
+    
+    @FXML
+    private BorderPane bPrincipal;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cargarPanel("triaje.fxml");
     }
+
+    @FXML
+    private void Registro(){
+        cargarPanel("triaje.fxml");
+    }
+    
+    
+    public void cargarPanel(String panel){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(panel));
+        try {
+            Parent root = loader.load();
+            
+            bPrincipal.setCenter(root);
+        } catch (IOException ex) {
+            System.out.println("Error al cargar en"+panel + ex);
+            ex.getMessage();
+        }
+    }
+   
 }
