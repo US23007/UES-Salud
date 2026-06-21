a8ca3626b3f1b9ae3a7db8eb416000d773474495
DMurgas <105890876+DMurgas@users.noreply.github.com>
2026-06-20 20:55:52 -0600

    Comentarios TriajeController.java

--- src/main/java/ues/ues/salud/TriajeController.java
+++ src/main/java/ues/ues/salud/TriajeController.java
@@ -1,6 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+/**
+ * Controlador del módulo de triaje
+ * encargado del apartado del formulario de triaje
+ * validación de todos los campos 
+ * Autor: David Murgas MH25061
  */
 package ues.ues.salud;
 
